6a4ec2f80ce4617855216d47a6979d31f870ae2d
Yepthatsit <karolgrzywa8@gmail.com>
2024-07-20 10:45:19 +0200

    deletd non necessery lines

--- plot_test.py
+++ plot_test.py
@@ -1,6 +1,4 @@
-from privte_python_projects.UniversalPlotter.UniversalPlotter import UniversalPlotter
-import asyncio
-from time import sleep
+from UniversalPlotter import UniversalPlotter
 def main()->None:
     plotter = UniversalPlotter('test.csv',['A,D','B,C','A,D','A,D'],coll_num=3)
     plotter.startPlot()
