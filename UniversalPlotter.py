fdb484e940686609b68fdbf1a54074cc239511a2
Yepthatsit <126805460+Yepthatsit@users.noreply.github.com>
2025-05-04 12:37:55 +0200

    Add files via upload
    asdasd

--- UniversalPlotter.py
+++ UniversalPlotter.py
@@ -14,7 +14,7 @@ class UniversalPlotter:
     axes:list = []
     lines:list = []
     sep = ""
-    def __init__(self,file:str,plots:list,coll_num:int = 2,separator:str = ',') -> None:
+    def __init__(self,file:str,plots:list,coll_num:int = 2,separator:str = ',',markersize = 0.1) -> None:
         """_summary_
         initialazes class with all necessery data. To start plot use startPlot method.
         Args:
@@ -26,13 +26,19 @@ class UniversalPlotter:
         self.filename = file
         self.plots_to_make = plots
         self.sep = separator
+        self.markersize = markersize
         columns = coll_num
         rows = math.ceil(len(self.plots_to_make)/columns)
         for i in range(len(self.plots_to_make)):
             subplot = self.figure.add_subplot(rows,columns,i+1)
-            line, = subplot.plot([],[],marker = 'o')
+            templines = []
+            plotparams = self.plots_to_make[i].split(',')
+            for j in range(1,len(plotparams)):
+                line, = subplot.plot([],[],marker = 'o',label = plotparams[j],markersize= self.markersize)
+                templines.append(line)
+                subplot.legend()
             self.axes.append(subplot)
-            self.lines.append(line)
+            self.lines.append(templines)
     def startPlot(self)-> None:
         """_summary_
         Starts a plot
@@ -46,16 +52,16 @@ class UniversalPlotter:
         data = pd.read_csv(self.filename,sep=self.sep)
         for i in range(len(self.plots_to_make)):
             plot_parameters = self.plots_to_make[i].split(',')
-            self.lines[i].set_data(data.loc[:,plot_parameters[0]],data.loc[:,plot_parameters[1]])
+            for j in range(1,len(plot_parameters)):
+                self.lines[i][j-1].set_data(data.loc[:,plot_parameters[0]],data.loc[:,plot_parameters[j]])
             self.axes[i].relim()
             self.axes[i].autoscale_view()
             self.axes[i].set_xlabel(plot_parameters[0])
-            self.axes[i].set_ylabel(plot_parameters[1])
+            self.axes[i].set_ylabel(" ".join(plot_parameters[1:]))
         self.figure.tight_layout()
 def main() -> None:
-    plot = UniversalPlotter(sys.argv[1],sys.argv[2:])
-    plot.startPlot()
-
-
+    #plot = UniversalPlotter(sys.argv[1],sys.argv[2:])
+    print("aaaa")
+    #plot.startPlot()
 if __name__ == "__main__":
     main()
